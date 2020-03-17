7d63f0ed05f5132438b09aab0cd91d311623e2af
William Fong <willfong@mit.edu>
2020-03-17 12:04:30 -0400

    Improving merge command to use interior numpy methods instead of manually iterating through the arrays ourselves

--- lightcurvedb/util/merge.py
+++ lightcurvedb/util/merge.py
@@ -7,28 +7,10 @@ else:
     rangefunc = xrange
 
 
-def matrix_merge(*matrices, **kwargs):
-    sort_row_index = kwargs.pop('sort_row_index', 0)
-    data = np.concatenate(matrices, axis=1)
-    reference_row = data[sort_row_index]
+def matrix_merge(arr1, arr2, **kwargs):
+    data = np.concatenate((np.copy(arr1), np.copy(arr2)), axis=1)
+    ref_row = data[0]
+    path = np.argsort(ref_row)
+    check = np.concatenate((np.diff(ref_row[path]), [1]))  # Always append last element
 
-    sort_path = np.argsort(reference_row)
-    shortened_path = []
-
-    for i in rangefunc(len(sort_path)):
-        try:
-            cur_index = sort_path[i]
-            next_index = sort_path[i+1]
-        except IndexError:
-            shortened_path.append(cur_index)
-            break
-
-        cur_data = reference_row[cur_index]
-        next_data = reference_row[next_index]
-        if cur_data == next_data:
-            # Skip duplicate value
-            continue
-        shortened_path.append(cur_index)
-
-    path = np.array(shortened_path)
-    return data[:,path]
+    return data[:,path[check > 0]]
