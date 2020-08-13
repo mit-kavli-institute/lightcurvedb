48c6d8cc382e22cfdcf08a6e35debee7cdf61d35
William Fong <willfong@mit.edu>
2020-08-13 13:56:58 -0400

    Adding new legacy methods to the observation model

--- lightcurvedb/models/observations.py
+++ lightcurvedb/models/observations.py
@@ -1,8 +1,12 @@
+import os
 from lightcurvedb.core.base_model import QLPModel
 
 from sqlalchemy import (Column, BigInteger, Integer, SmallInteger, ForeignKey, bindparam)
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import insert
+
+
+LEGACY_QLP_DIR = '/pdo/qlp-data'
 
 
 class Observation(QLPModel):
@@ -38,3 +42,51 @@ class Observation(QLPModel):
         )
 
         return q
+
+    def expected_orbit_h5_path(self, qlp_base_dir=LEGACY_QLP_DIR):
+        """
+        Determine the expected orbit h5 filepath for this observation.
+
+        Parameters
+        ----------
+        qlp_base_dir : str or Path-like
+            The filepath prefix to all QLP orbits. Defaults to ``/pdo/qlp-data/``
+        """
+        h5_basename = '{}.h5'.format(self.tic_id)
+        data_prefix = os.path.join(
+            'orbit-{}'.format(self.orbit.orbit_number),
+            'ffi',
+            'cam{}'.format(self.camera),
+            'ccd{}'.format(self.ccd),
+            'LC'
+        )
+        return os.path.join(
+            qlp_base_dir, data_prefix, h5_basename
+        )
+
+    def expected_sector_h5_path(self, qlp_base_dir=LEGACY_QLP_DIR):
+        """
+        Determine the expected sector h5 filepath for this observation.
+
+        Parameters
+        ----------
+        qlp_base_dir : str or Path-like
+            The filepath prefix to all QLP sectors. Defaults to ``/pdo/qlp-data/``
+
+        Notes
+        -----
+        This method assumes that all orbit data is aligned by sector. So long
+        as the definition of a sector remains, this method will work.
+        """
+        h5_basename = '{}.h5'.format(self.tic_id)
+        data_prefix = os.path.join(
+            'sector-{}'.format(self.orbit.sector),
+            'ffi',
+            'cam{}'.format(self.camera),
+            'ccd{}'.format(self.ccd),
+            'LC'
+        )
+        return os.path.join(
+            qlp_base_dir, data_prefix, h5_basename
+        )
+
