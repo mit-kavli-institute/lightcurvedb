9f672b3b6971c233e3d91d1d6fac08390e6c8e47
William Fong <willfong@mit.edu>
2020-10-23 16:41:52 -0400

    adding helper installation scripts to streamline pdodev requirements

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/bash
 
+PIP_PATH=${1:-"pip"}
+
 current_branch=$(git branch | sed -n '/\* /s///p')
 lightcurvedb_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"  # Will fail on symlinked directories!
 
-echo "Installing lightcurvedb on branch ${current_branch}"
-
-git remote update
 cd $lightcurvedb_path
-pip install --user .
+$PIP_PATH install --user .
--- pdo_user_install.sh
+++ pdo_user_install.sh
@@ -1,7 +1,29 @@
 #! /usr/bin/bash
 
+if [ "$1" = "help" ] || [ "$1" = "--help" ] ; then
+    echo "./pdo_user_install.sh [PIP VERSION]"
+    echo "\tPIP VERSION defaults to 3"
+    exit 1
+fi
+
+PIP_VERSION=${1:-"3"}
+PIP_PATH=$(which "pip${PIP_VERSION}" 2> /dev/null )
+
+if [ -z $PIP_PATH ] ; then
+    echo "Could not find pip version ${PIP_VERSION}!"
+    exit 1
+fi
+
 current_branch=$(git branch | sed -n '/\* /s///p')
 lightcurvedb_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"  # Will fail on symlinked directories!
 script_path="${lightcurvedb_path}/install.sh"
-echo "Installing lightcurvedb: ${current_branch} via pdodev with ${script_path}"
-ssh -t pdodev "./${script_path}"
+echo "Installing lightcurvedb: ${current_branch} via pdodev using pip${PIP_VERSION}"
+ssh -t pdodev PATH=${PATH} PYTHONPATH=${PYTHONPATH} << EOF
+    . /etc/bashrc
+    . /sw/bin/setup-pdo.sh
+    export LD_LIBRARY_PATH=/sw/openssl-versions/openssl-1.1.1d/lib:/sw/python-versions/python-3.7.5/lib/:$LD_LIBRARY_PATH
+    export PATH=/sw/python-versions/python-3.7.5/bin/:$PATH
+    export PATH=/sw/cmake-versions/cmake-3.15.3/bin/:$PATH
+    cd ${lightcurvedb_path}
+    ./install.sh ${PIP_PATH}
+EOF
