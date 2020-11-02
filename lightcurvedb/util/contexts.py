cdcd3504ea2a320662a93e9a3cc7f4a107146843
William Fong <willfong@mit.edu>
2020-11-02 09:52:18 -0700

    linting and formatting

--- lightcurvedb/util/contexts.py
+++ lightcurvedb/util/contexts.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 
 CONTEXTS = []
@@ -28,3 +29,15 @@ def extract_pdo_path_context(path):
 REGISTER(r"orbit-(?P<orbit_number>[0-9]+)")
 REGISTER(r"cam(?P<camera>[1-4])")
 REGISTER(r"ccd(?P<ccd>[1-4])")
+
+
+try:
+    from pathlib import Path
+
+    def get_parent_dir(path):
+        return Path(path).parts[-1]
+
+except ImportError:
+
+    def get_parent_dir(path):
+        return os.path.basename(os.path.dirname(path))
