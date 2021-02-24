1dbc4d9f9db73a06111f109101fdcfac8725d872
William Fong <willfong@mit.edu>
2021-02-24 11:49:50 -0500

    Bumping versioning

--- lightcurvedb/__init__.py
+++ lightcurvedb/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 from lightcurvedb.core.connection import DB, db_from_config
 from lightcurvedb.core.engines import __DEFAULT_PATH__, __SESSION_FACTORY__
