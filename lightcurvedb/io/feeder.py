32b38b369274db358889d4acb88bfda31c3330ca
William Fong <willfong@mit.edu>
2021-07-15 18:09:48 +0000

    Standardizing output

--- lightcurvedb/io/feeder.py
+++ lightcurvedb/io/feeder.py
@@ -63,7 +63,7 @@ class LightcurveFeeder(Process):
                     result["error"] = "No lightpoints found, empty lightcurve"
                 except PrimaryIdentNotFound:
                     result = {
-                        "error": "No lightcurve found for identifier {0}.".format(id_),
+                        "error": "No lightcurve found for identifier {0}".format(id_),
                         "identity": id_
                     }
                 except Exception as e:
