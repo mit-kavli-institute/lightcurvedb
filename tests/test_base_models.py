61d84b331097f1da7cd2e3d3445b702ca91eb90c
William Fong <willfong@mit.edu>
2021-11-02 15:32:03 -0400

    more flake8 corrections

--- tests/test_base_models.py
+++ tests/test_base_models.py
@@ -1,12 +1,10 @@
-from sqlalchemy import text
-
 from lightcurvedb.core.admin import psql_tables
 from lightcurvedb.models import Lightcurve, Orbit
 
-from .fixtures import clear_all, db_conn
+from .fixtures import clear_all, db_conn  # noqa F401
 
 
-def test_get_oid(db_conn):
+def test_get_oid(db_conn):  # noqa F401
     with db_conn as db:
         psql_tables(db)
         assert db.query(Lightcurve.oid) is not None
@@ -14,4 +12,4 @@ def test_get_oid(db_conn):
 
 def test_get_orbit_properties():
     property, contexts = Orbit.get_property("orbit_number")
-    assert not "join_contexts" in contexts
+    assert "join_contexts" not in contexts
