1520c1ee1997c0046db49ef46130ce3cc4172332
William Fong <willfong@mit.edu>
2022-03-15 13:15:28 -0400

    Experimenting with working temp tables

--- lightcurvedb/experimental/temp_table.py
+++ lightcurvedb/experimental/temp_table.py
@@ -7,10 +7,40 @@ be made.
 
 from contextlib import ContextDecorator
 from loguru import logger
-from sqlalchemy import Table, Column, orm, exc as sqlexc
+from sqlalchemy import Table, Column, MetaData, exc as sqlexc, event, schema, types
 
 
-TempBase = orm.declarative_base()
+_tempmeta = MetaData()
+
+_SQL_ALIASES = {
+    "bigint": types.BigInteger,
+    "biginteger": types.BigInteger,
+    "bool": types.Boolean,
+    "boolean": types.Boolean,
+    "date": types.Date,
+    "datetime": types.DateTime,
+    "float": types.Float,
+    "int": types.Integer,
+    "integer": types.Integer,
+    "interval": types.Interval,
+    "largebinary": types.LargeBinary,
+    "numeric": types.Numeric,
+    "pickle": types.PickleType,
+    "pickletype": types.PickleType,
+    "smallint": types.SmallInteger,
+    "smallinteger": types.SmallInteger,
+    "text": types.Text,
+    "time": types.Time,
+}
+
+
+def _str_to_sql_type(string):
+    try:
+        return _SQL_ALIASES[string.lower()]
+    except KeyError:
+        raise KeyError(
+            f"Unknown specified type {string}"
+        )
 
 
 class TempTable(ContextDecorator):
@@ -27,7 +57,6 @@ class TempTable(ContextDecorator):
         t.insert(col2=2)
         print(open_db_inst.query(t.col2).all())
     """
-    _columns = {}
     _db = None
     _table = None
 
@@ -45,7 +74,13 @@ class TempTable(ContextDecorator):
             The name for this table.
         """
         self._db = db
-        self._table = Table(name, TempBase, prefixes=["TEMPORARY"])
+        self._table = Table(name, _tempmeta, prefixes=["TEMPORARY"])
+
+    def __getattr__(self, name):
+        return getattr(self.table, name)
+
+    def __getitem__(self, key):
+        return self.table.c[key]
 
     def __enter__(self):
         """
@@ -54,8 +89,8 @@ class TempTable(ContextDecorator):
         healthy connection. This method will raise an error if an attempt to
         create a table without an open connection is made.
         """
-        engine = self._db.session.engine
-        self._table.create(bind=engine)
+        ddl = schema.CreateTable(self._table)
+        self._db.execute(ddl)
         return self
 
     def __exit__(self, exc_type, exc, exc_info):
@@ -69,8 +104,6 @@ class TempTable(ContextDecorator):
                 "rolling back database transaction"
             )
             self._db.rollback()
-        self._table.drop()
-
         return exc_type is None
 
     def add_column(self, name, type_, **kwargs):
@@ -84,7 +117,11 @@ class TempTable(ContextDecorator):
         type_ : sqlalchemy.Type
 
         """
-        column = Column(name, type_, **kwargs)
+        if isinstance(type_, str):
+            sql_type = _str_to_sql_type(type_)
+        else:
+            sql_type = type_
+        column = Column(name, sql_type, **kwargs)
         self._table.append_column(column)
 
     def insert(self, **values):
@@ -93,3 +130,27 @@ class TempTable(ContextDecorator):
         """
         stmt = self._table.insert().values(**values)
         return self._db.execute(stmt)
+
+    def insert_many(self, values, scalar=False):
+        """
+        Bulk insert the given values.
+
+        Parameters
+        ----------
+        values: iter
+            An iterable of values to insert in one bulk operation.
+        scalar: bool
+            If true, wrap the values iterable with (val, ). This is for easy
+            insertion of a temp table with a single column.
+        """
+        stmt = self._table.insert()
+        if scalar:
+            stmt = stmt.values(list((val,) for val in values))
+        else:
+            stmt = stmt.values(list(values))
+
+        return self._db.execute(stmt)
+
+    @property
+    def table(self):
+        return self._table
