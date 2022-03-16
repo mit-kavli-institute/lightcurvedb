0ed91edde8816eca1019930572af7c3825086b9f
William Fong <willfong@mit.edu>
2022-03-16 18:32:40 -0400

    removing dup SQL_TYPE resolver

--- lightcurvedb/experimental/temp_table.py
+++ lightcurvedb/experimental/temp_table.py
@@ -8,39 +8,10 @@ be made.
 from contextlib import ContextDecorator
 from loguru import logger
 from sqlalchemy import Table, Column, MetaData, exc as sqlexc, event, schema, types
+from lightcurvedb.core.sql import _str_to_sql_type
 
 
 _tempmeta = MetaData()
-
-_SQL_ALIASES = {
-    "bigint": types.BigInteger,
-    "biginteger": types.BigInteger,
-    "bool": types.Boolean,
-    "boolean": types.Boolean,
-    "date": types.Date,
-    "datetime": types.DateTime,
-    "float": types.Float,
-    "int": types.Integer,
-    "integer": types.Integer,
-    "interval": types.Interval,
-    "largebinary": types.LargeBinary,
-    "numeric": types.Numeric,
-    "pickle": types.PickleType,
-    "pickletype": types.PickleType,
-    "smallint": types.SmallInteger,
-    "smallinteger": types.SmallInteger,
-    "text": types.Text,
-    "time": types.Time,
-}
-
-
-def _str_to_sql_type(string):
-    try:
-        return _SQL_ALIASES[string.lower()]
-    except KeyError:
-        raise KeyError(
-            f"Unknown specified type {string}"
-        )
 
 
 class TempTable(ContextDecorator):
