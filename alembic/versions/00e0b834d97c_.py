9acf099789ebcb42f2e901a080482f903eba8951
William Fong <willfong@mit.edu>
2022-03-22 14:17:53 -0400

    needing to merge alembic heads

---
+++ alembic/versions/00e0b834d97c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 00e0b834d97c
+Revises: efb12c5fb79c, be86fdf5cef3
+Create Date: 2022-03-22 14:17:25.824888
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "00e0b834d97c"
+down_revision = ("efb12c5fb79c", "be86fdf5cef3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
