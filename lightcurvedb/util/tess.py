2583c42521d067707edd35b90e2e24f7ddfdc121
William Fong <willfong@mit.edu>
2022-06-02 11:15:56 -0400

    addressing https://tessgit.mit.edu/wcfong/lightcurve-database/-/merge_requests/53#note_7722

--- lightcurvedb/util/tess.py
+++ lightcurvedb/util/tess.py
@@ -2,9 +2,10 @@
 This module provides utility functions to TESS specific contexts.
 """
 
+
 def sector_from_orbit_number(orbit_number: int) -> int:
     return ((orbit_number + 1) // 2) - 4
 
 
-def orbit_number_from_sector(sector: int) -> int:
-    return (2 * sector) + 7
+def orbit_numbers_from_sector(sector: int) -> int:
+    return (2 * sector) + 7, (2 * sector) + 8
