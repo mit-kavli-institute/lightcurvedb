f0f327abaa536ffeab135b6f9776df70483c44d9
William Fong <willfong@mit.edu>
2022-09-14 10:09:06 -0500

    fixing bad query construction

--- lightcurvedb/managers/bestlightcurves.py
+++ lightcurvedb/managers/bestlightcurves.py
@@ -79,7 +79,7 @@ def _baked_best_lightcurve(db_config, tic_id):
                 Lightpoint.y_centroid_array(),
                 Lightpoint.quality_flag_array(),
             )
-            .join(OrbitLightcurve.id == Lightpoint.lightpoint_id)
+            .join(OrbitLightcurve.id == Lightpoint.lightcurve_id)
             .filter(Lightpoint.lightcurve_id.in_(ids))
         )
         result = q.one()
