b25126c4e5b64f66e83c1d05b2ee981ab19e4cf8
William Fong <willfong@mit.edu>
2022-10-16 21:05:17 -0600

    fixing runtime error

--- lightcurvedb/io/feeder.py
+++ lightcurvedb/io/feeder.py
@@ -220,7 +220,7 @@ def fetch_best_orbit_baseline(db, job):
         )
         .where(lc.tic_id == tic_id)
     )
-    ids = [id for id, in q]
+    ids = [id for id, in db.execute(q)]
 
     lp_q = (
         sa.select(*[getattr(m.Lightpoint, col) for col in columns])
