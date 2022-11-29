e1cac5c767a32e5be62eb9c20c057cf1d3d1b7e1
William Fong <willfong@mit.edu>
2022-11-29 12:30:13 -0700

    safer type check

--- lightcurvedb/managers/bestlightcurves.py
+++ lightcurvedb/managers/bestlightcurves.py
@@ -61,7 +61,9 @@ class BestLightcurveManager(LightcurveManager):
             m.BestOrbitLightcurve,
             m.BestOrbitLightcurve.lightcurve_join(m.ArrayOrbitLightcurve),
         )
-        if isinstance(tic_ids, int) or len(tic_ids) == 1:
+        if isinstance(tic_ids, int):
+            q = q.where(m.ArrayOrbitLightcurve.tic_id == tic_ids)
+        elif len(tic_ids) == 1:
             q = q.where(m.ArrayOrbitLightcurve.tic_id == list(tic_ids)[0])
         else:
             q = q.where(m.ArrayOrbitLightcurve.tic_id.in_(tic_ids))
