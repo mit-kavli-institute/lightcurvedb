5f825c319383223e0f658bb609095dee1fed9abf
William Fong <willfong@mit.edu>
2022-12-08 11:11:44 -0500

    updating provisioning

--- lightcurvedb/core/partitioning.py
+++ lightcurvedb/core/partitioning.py
@@ -2,6 +2,7 @@
 This module describes partitioning of the lightcurve database.
 """
 import sqlalchemy as sa
+from loguru import logger
 
 from lightcurvedb.core.connection import db_from_config
 
@@ -10,6 +11,8 @@ def reorder_chunk(db_config, chunkpath, index):
     reorder_q = sa.select().select_from(
         sa.func.reorder_chunk(chunkpath, index)
     )
+    logger.debug(f"Clustering {chunkpath}")
     with db_from_config(db_config, isolation_level="AUTOCOMMIT") as db:
         db.execute(reorder_q)
+    logger.debug(f"Finished clustering {chunkpath}")
     return chunkpath
