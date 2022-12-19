5145d17fd0b879b54a603b095cf3335658869c30
William Fong <willfong@mit.edu>
2022-12-19 08:51:04 -0700

    removing imports for consistency

--- lightcurvedb/__init__.py
+++ lightcurvedb/__init__.py
@@ -1,12 +1,8 @@
 __version__ = "0.15.0a"
 
 from lightcurvedb.core.connection import db, db_from_config
-from lightcurvedb.models import Aperture, LightcurveType, Orbit
 
 __all__ = [
-    "Aperture",
-    "LightcurveType",
-    "Orbit",
     "db_from_config",
     "db",
 ]
