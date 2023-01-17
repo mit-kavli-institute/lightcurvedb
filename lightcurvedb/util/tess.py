631f16bb24b77c7c97dc843ea8ae63ed5a9694f7
William Fong <willfong@mit.edu>
2023-01-17 12:11:41 -0700

    adding quick gps time -> utc conversion

--- lightcurvedb/util/tess.py
+++ lightcurvedb/util/tess.py
@@ -1,6 +1,9 @@
 """
 This module provides utility functions to TESS specific contexts.
 """
+from datetime import datetime
+
+from astropy.time import Time
 
 
 def sector_from_orbit_number(orbit_number: int) -> int:
@@ -9,3 +12,21 @@ def sector_from_orbit_number(orbit_number: int) -> int:
 
 def orbit_numbers_from_sector(sector: int) -> int:
     return (2 * sector) + 7, (2 * sector) + 8
+
+
+def gps_time_to_datetime(gps_time: float) -> datetime:
+    """
+    Convert a floating point gps timestamp to a python datetime object.
+
+    Parameters
+    ----------
+    gps_time: float
+        The recorded gps time to convert
+
+    Returns
+    -------
+    datetime
+        The converted time.
+    """
+    t = Time(gps_time, format="gps")
+    return Time(t, format="isot", format="utc").datetime
