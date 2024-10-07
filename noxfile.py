eaa5bb6dea30fdb656a489c5ba5bc90f01c031d8
William Fong <willfong@mit.edu>
2024-10-07 12:42:50 -0600

    correctly installing nox session dependencies

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,7 @@ from nox.sessions import Session
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def property_tests(session):
-    spec = nox.project.load_toml("pyproject.toml")
-    project_requirements = spec["project"]["dependencies"]
-    test_requirements = spec["project"]["optional-dependencies"]["dev"]
-    session.install(*project_requirements)
-    session.install(*test_requirements)
+    session.install(".[dev]")
     flags = session.posargs if session.posargs else []
     session.run("pytest", *flags)
 
