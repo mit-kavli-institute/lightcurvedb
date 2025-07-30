54096dd5ca27d0262098cc5e5c6fedcaebafd11c
William Fong <willfong@mit.edu>
2025-07-30 09:05:10 -0600

    migrating dependencies to use private index, removing unused import

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,11 @@ from nox.sessions import Session
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def property_tests(session):
-    session.install(".[dev]")
+    session.install(
+        ".[dev]",
+        "--extra-index-url",
+        "https://mit-kavli-institute.github.io/MIT-Kavli-PyPi/",
+    )
     flags = session.posargs if session.posargs else []
     session.run("pytest", *flags)
 
@@ -14,7 +18,11 @@ def docs(session: Session):
     spec = nox.project.load_toml("pyproject.toml")
     requirements = spec["project"]["optional-dependencies"]["docs"]
     # Install the package in editable mode with all dependencies
-    session.install("-e", ".")
+    session.install(
+        ".[dev]",
+        "--extra-index-url",
+        "https://mit-kavli-institute.github.io/MIT-Kavli-PyPi/",
+    )
     session.install(*requirements)
 
     session.run(
