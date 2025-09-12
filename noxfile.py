10c224b93fd8b7e57df16e60e75a17d9e27bd2e2
William Fong <willfong@mit.edu>
2025-09-12 11:12:46 -0600

    correcting project requirements

--- noxfile.py
+++ noxfile.py
@@ -2,19 +2,22 @@ import nox
 from nox.sessions import Session
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.11", "3.12"])
 def property_tests(session):
-    session.install(".[dev]")
+    session.install(
+        ".[dev]",
+        "--extra-index-url",
+        "https://mit-kavli-institute.github.io/MIT-Kavli-PyPi/",
+    )
     flags = session.posargs if session.posargs else []
     session.run("pytest", *flags)
 
 
-@nox.session(python=["3.9"])
+@nox.session(python=["3.11"])
 def docs(session: Session):
-    spec = nox.project.load_toml("pyproject.toml")
-    project_requirements = spec["project"]["dependencies"]
-    requirements = spec["project"]["optional-dependencies"]["docs"]
-    session.install(*project_requirements)
-    session.install(*requirements)
-
+    session.install(
+        ".[docs]",
+        "--extra-index-url",
+        "https://mit-kavli-institute.github.io/MIT-Kavli-PyPi/",
+    )
     session.run("sphinx-build", "-M", "html", "docs/source/", "docs/build/")
