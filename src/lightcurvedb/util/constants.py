66079a24cf8d81bcc1b4a5338a2474bba14b7033
William Fong <willfong@mit.edu>
2025-10-28 08:49:39 -0600

    removing older utility code

--- src/lightcurvedb/util/constants.py
+++ src/lightcurvedb/util/constants.py
@@ -1,9 +1,5 @@
 import pathlib
-from datetime import datetime
 
-TESS_FIRST_LIGHT = datetime(2018, 8, 7)
-GPS_LEAP_SECONDS = 18
 DEFAULT_CONFIG_PATH = pathlib.Path(
     "~", ".config", "lightcurvedb", "db.conf"
 ).expanduser()
-TIC8_TEMPLATE = "{dialect}://{username}:{password}@{host}:{port}/{database}"
