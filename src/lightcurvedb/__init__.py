db05ca5bc0ea20976f7cfaa339de43996ec45ca2
semantic-release <semantic-release>
2026-03-12 15:25:09 +0000

    3.0.0
    Automatically generated by python-semantic-release

--- src/lightcurvedb/__init__.py
+++ src/lightcurvedb/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.0.0-beta.7"  # Managed by python-semantic-release
+__version__ = "3.0.0"  # Managed by python-semantic-release
 
 from lightcurvedb.core.connection import LCDB_Session, db, db_from_config
 
